1b7e86d6ffced11e7b69a781665f167083710517
Samita <samita.khanduri.qu@gmail.com>
2025-11-29 19:12:39 +0530

    minor changes in MainAapplication kt

--- android/app/src/main/java/com/yourcompany/pulseapp/MainApplication.kt
+++ android/app/src/main/java/com/yourcompany/pulseapp/MainApplication.kt
@@ -23,13 +23,12 @@ class MainApplication : Application(), ReactApplication {
   override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
       this,
       object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
-              packages.add(PulsePackage())
-              return packages
-            }
+        override fun getPackages(): List<ReactPackage> {
+          val packages = PackageList(this).packages.toMutableList()
+          packages.add(PulsePackage())
+          return packages
+}
+
 
           override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
 
